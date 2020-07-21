a0b32f20bccca24a745f4edef5868b156456db02
Teo Podobnik <tp4348@student.uni-lj.si>
2020-07-21 11:30:29 +0200

    Apparently running tests works.

--- test/test_goal_queueing.cpp
+++ test/test_goal_queueing.cpp
@@ -1,44 +1,50 @@
+#include <gtest/gtest.h>
 #include <ros/ros.h>
 #include <actionlib/client/simple_action_client.h>
 #include <actionlib/client/terminal_state.h>
 #include <move_base_msgs/MoveBaseAction.h>
-#include <gtest/gtest.h>
-
+#include <move_basic/queued_action_server.h>
 
 class GoalQueueSuite : public ::testing::Test {
 protected:
-	virtual void SetUp() {}
+	virtual void SetUp() {
+		// variables here?
+	}
 	virtual void TearDown() {}
+public:
+	GoalQueueSuite() {}
+	void executeCallback(const move_base_msgs::MoveBaseGoalConstPtr &msg){
+		ROS_INFO("Working.");
+	}
 };
 
 
-TEST_F(GoalQueueSuite, executionDelay) {	
-	actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> cli("move_base", true); // true -> don't need ros::spin(), required separate thread
+TEST_F(GoalQueueSuite, executionDelay) {
+	ros::NodeHandle nh;
+	actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> cli("queue_server", true); // true -> don't need ros::spin(), required separate thread
+	actionlib::QueuedActionServer<move_base_msgs::MoveBaseAction> qserv(nh, "queue_server", boost::bind(&GoalQueueSuite::executeCallback, this, _1));	
+
+	// defining a goal
+	move_base_msgs::MoveBaseGoal goal; // goal type not okay?
+	goal.target_pose.pose.position.x = 3.0;
+	goal.target_pose.pose.position.y = 4.0; 
+	goal.target_pose.pose.orientation.x = 1.0;	
+	goal.target_pose.pose.orientation.y = 1.0;
+	goal.target_pose.pose.orientation.z = 1.0;
+	goal.target_pose.pose.orientation.w = 1.0;
+	qserv.goalCallback(goal);
+	/*
 	while(!cli.waitForServer(ros::Duration(5.0))){ROS_INFO("Waiting for the move_base action server to come up");}
 	if (cli.getState() == actionlib::SimpleClientGoalState::PENDING) { // PENDING = 0
-		move_base_msgs::MoveBaseGoal goal;
-		goal.target_pose.header.frame_id = "base_link";	
-		goal.target_pose.header.stamp = ros::Time::now();
-		goal.target_pose.pose.position.x = 3.0;
-		goal.target_pose.pose.position.y = 4.0; 
-		goal.target_pose.pose.orientation.x = 1.0;	
-		goal.target_pose.pose.orientation.y = 1.0;
-		goal.target_pose.pose.orientation.z = 1.0;
-		goal.target_pose.pose.orientation.w = 1.0;
-		ROS_DEBUG("Goal created.");	
-		cli.sendGoal(goal);
-		EXPECT_EQ(cli.getState(), actionlib::SimpleClientGoalState::ACTIVE); // Get the state information about the current goal
+	actionlib::SimpleClientGoalState state = cli.getState();
+    	ROS_INFO("Action finished: %s",state.toString().c_str());
+	*/
 	}
+
+
+int main(int argc, char **argv) {
+    ros::init(argc, argv, "goal_queueing_test");
+    //ros::start();
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
-
-// Five more TEST_F and a PITFALL missing
-
-int main(int argc, char** argv) {
-	ros::init(argc, argv, "goal_queueing_test");
-	testing::InitGoogleTest(&argc, argv);
-	//return (RUN_ALL_TESTS() ? "Testing failed." : "Testing successful.");
-	return RUN_ALL_TESTS();
-}
-
-
-
