2bca8e3ddd95baff1a071cc268f813b15700c8e2
Teo Podobnik <tp4348@student.uni-lj.si>
2020-07-21 22:08:06 +0200

    First test bug.

--- test/test_goal_queueing.cpp
+++ test/test_goal_queueing.cpp
@@ -9,42 +9,38 @@ class GoalQueueSuite : public ::testing::Test {
 protected:
 	virtual void SetUp() {
 		// variables here?
+	//initialization, declaration!!!
 	}
 	virtual void TearDown() {}
 public:
-	GoalQueueSuite() {}
+	move_base_msgs::MoveBaseGoalConstPtr current_goal;	
 	void executeCallback(const move_base_msgs::MoveBaseGoalConstPtr &msg){
-		ROS_INFO("Working.");
+		current_goal = msg;
 	}
 };
 
 
-TEST_F(GoalQueueSuite, executionDelay) {
+TEST_F(GoalQueueSuite, establishDuplex) {
+	// TODO:  Make this more beautiful
 	ros::NodeHandle nh;
 	actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> cli("queue_server", true); // true -> don't need ros::spin(), required separate thread
 	actionlib::QueuedActionServer<move_base_msgs::MoveBaseAction> qserv(nh, "queue_server", boost::bind(&GoalQueueSuite::executeCallback, this, _1));	
 
-	// defining a goal
-	move_base_msgs::MoveBaseGoal goal; // goal type not okay?
+	move_base_msgs::MoveBaseGoal goal; 
 	goal.target_pose.pose.position.x = 3.0;
-	goal.target_pose.pose.position.y = 4.0; 
-	goal.target_pose.pose.orientation.x = 1.0;	
-	goal.target_pose.pose.orientation.y = 1.0;
-	goal.target_pose.pose.orientation.z = 1.0;
-	goal.target_pose.pose.orientation.w = 1.0;
-	qserv.goalCallback(goal);
-	/*
-	while(!cli.waitForServer(ros::Duration(5.0))){ROS_INFO("Waiting for the move_base action server to come up");}
-	if (cli.getState() == actionlib::SimpleClientGoalState::PENDING) { // PENDING = 0
-	actionlib::SimpleClientGoalState state = cli.getState();
-    	ROS_INFO("Action finished: %s",state.toString().c_str());
-	*/
-	}
+	cli.sendGoal(goal);
+	ros::Duration(5.0).sleep();
+	ASSERT_EQ(3.0, current_goal->target_pose.pose.position.x);
+	
+	// ROS API
+	//ASSERT_TRUE(cli.getState() == actionlib::SimpleClientGoalState::ACTIVE ? true : false);
+	//actionlib::SimpleClientGoalState state = cli.getState();
+
+}
 
 
 int main(int argc, char **argv) {
     ros::init(argc, argv, "goal_queueing_test");
-    //ros::start();
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
