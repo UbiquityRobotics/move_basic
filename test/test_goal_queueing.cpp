62227a3c22a654a546dccd5f16ea8952f2d8b6d0
Teo Podobnik <tp4348@student.uni-lj.si>
2020-07-22 21:54:46 +0200

    Flags setup.

--- test/test_goal_queueing.cpp
+++ test/test_goal_queueing.cpp
@@ -8,45 +8,45 @@
 
 class GoalQueueSuite : public ::testing::Test {
 protected:
-	virtual void SetUp() {
-		got_goal = false;
-		goal_preempted = false;
-		current_goal = NULL;
-		next_goal = NULL;
-	}
+	virtual void SetUp() {}
 	virtual void TearDown() {}
-public:	
+
+	//SHARED POINTER
+
 	bool got_goal;
 	bool goal_preempted;
 	move_base_msgs::MoveBaseGoalConstPtr current_goal;
 	move_base_msgs::MoveBaseGoalConstPtr next_goal;
-	//std::queue<move_base_msgs::MoveBaseGoal> qgoals;	
 	ros::NodeHandle nh;
 	actionlib::QueuedActionServer<move_base_msgs::MoveBaseAction> qserv;
 	actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> cli;	
 
-	GoalQueueSuite() : qserv(nh, "queue_server", boost::bind(&GoalQueueSuite::executeCallback, this, _1)),		   
+	// TODO: Sharing between objects? (Isolation!)
+	GoalQueueSuite() : got_goal(false),
+			   goal_preempted(false),
+			   current_goal(nullptr),
+			   next_goal(nullptr),
+			   qserv(nh, "queue_server", boost::bind(&GoalQueueSuite::executeCallback, this, _1)),		   
 			   cli("queue_server", true) // true -> don't need ros::spin(), required separate thread 
 	{
 		qserv.start();
 	}
+	// ADD GOALCALLBACK
 
 	~GoalQueueSuite() {
 		qserv.shutdown();
 	}
 
+	// TODO: Making sure it gets called at the right time (flags!)
 	void executeCallback(const move_base_msgs::MoveBaseGoalConstPtr &msg){
 		got_goal = true;
-		if (current_goal == NULL) current_goal = msg;
-		else if (next_goal == NULL) next_goal = msg;
+		if (current_goal == nullptr) current_goal = msg;
+		else if (next_goal == nullptr) next_goal = msg;
 		else {
-			qserv.setPreempted();
-			goal_preempted = true; //needed?
+			current_goal = next_goal;
+			goal_preempted = true; 
+			next_goal = msg;
 		} 
-		// TODO: Queueing behaviour
-		//if (qgoals.size() > 2) { // start index?
-		//	qgoals.push(*current_goal);
-		//}		
 	}
 };
 
@@ -56,22 +56,19 @@ TEST_F(GoalQueueSuite, establishDuplex) {
 	goal.target_pose.pose.position.x = 3.0;
 	cli.sendGoal(goal);
 
-	ros::spinOnce();
-	ros::Duration(1.0).sleep();
+	ros::spinOnce(); 
+	ros::Duration(1.0).sleep(); // TODO: Make this disappear 
 
 	ASSERT_TRUE(got_goal);
 	ASSERT_EQ(3.0, current_goal->target_pose.pose.position.x);
-	
-	// ROS API
 }
 
 
-TEST_F(GoalQueueSuite, queueAdding) { // probably now I should be sending to goalCallback
+TEST_F(GoalQueueSuite, queueAdding) { // Probably now I should be sending to goalCallback?
 	/* 
 	- if another goal is received add it to the queue
 	- if the queue full, set the current goal as preempted
 	- start executing the next goal in queue (the one after the preempted)
-	- 
 	*/	
 
 	//ASSERT_TRUE(qserv.isPreemptRequested());
