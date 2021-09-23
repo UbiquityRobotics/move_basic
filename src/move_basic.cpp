675a32a35a9690d98348b34bcdd70f783ce32787
MoffKalast <vid.rijavec@gmail.com>
2021-09-23 14:09:16 +0000

    removed linear rotation part

--- src/move_basic.cpp
+++ src/move_basic.cpp
@@ -754,10 +754,10 @@ bool MoveBasic::moveLinear(tf2::Transform& goalInDriving,
         /* Since we are dealing with imperfect localization we should make
          * sure we are at least runawayTimeout driving away from the goal*/
         double angleRemaining = std::atan2(remaining.y(), remaining.x());
-        if (std::cos(angleRemaining) < 0) { // 0
+        if (std::cos(angleRemaining) < 0) {
             if (ros::Time::now() - last > runawayTimeout) {
                 ROS_INFO("MoveBasic: Moving away from goal");
-                sendCmd(rotation, 0);
+                sendCmd(0, 0);
                 return true;
             }
         }
